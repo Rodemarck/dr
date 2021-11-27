b8e0a57afa932c61f5606f7bbae6e5386f058d4d
Rodemarck <38978758+Rodemarck@users.noreply.github.com>
2021-11-26 22:24:31 -0300

    comite final

---
+++ vuforia/Assets/rode/codigo/Fala.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace rode.codigo
+{
+    public class Fala
+    {
+        public string nome;
+        public string texto;
+        public Dictionary<string, int> opcoes;
+    }
+}
